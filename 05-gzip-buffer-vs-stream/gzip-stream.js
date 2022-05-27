2d58f86055727e4d9bd6f2bf20316f10bab376f6
Maximilian Schmidt <maximilian5189@posteo.de>
2022-05-27 10:48:16 +0200

    changes streaming example

--- 05-gzip-buffer-vs-stream/gzip-stream.js
+++ 05-gzip-buffer-vs-stream/gzip-stream.js
@@ -1,16 +1,26 @@
 import { createReadStream, createWriteStream } from "fs"
 import { createGzip } from "zlib"
+import { pipeline } from "stream"
 
 const filename = process.argv[2]
 
-// // pipe: readable -> writable
+// pipeline(
+//   createReadStream(filename), // readable
+//   createGzip(), // transfrom
+//   createWriteStream(`${filename}.gz`), // writable
+//   (err) => {
+//     if (err) console.log(err)
+//   }
+// ).on("finish", () => console.log("File successfully compressed"))
+
+// pipe: readable -> writable
 // createReadStream(filename) // readable
 //   .pipe(createGzip()) // transfrom
 //   .pipe(createWriteStream(`${filename}.gz`)) // writable
-//   .on('finish', () => console.log('File successfully compressed'))
-//   .on('error', () => console.log("error!"))
+//   .on("finish", () => console.log("File successfully compressed"))
+//   .on("error", () => console.log("error!"))
 
-const fileStream = createReadStream(filename).on("error", (err) => console.log("error!", err))
+const fileStream = createReadStream(filename)
 const gzipStream = fileStream.pipe(createGzip())
 const writeStream = gzipStream.pipe(createWriteStream(`${filename}.gz`))
 writeStream.on("finish", () => console.log("File successfully compressed")) // auf den writeStream subscriben; hier sieht man, dass Stream EventEmitter ist
